4ac3edf2a740dab456a0c7374339192a514361af
fengtao.xue <fengtao.xue@gausscode.com>
2018-01-26 14:52:23 +0800

    init hbase-devp

---
+++ hbase-main/src/main/java/com/rbs/cn/main/HbDemo.java
@@ -0,0 +1,36 @@
+package com.rbs.cn.main;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
+import utils.HBaseHelper;
+
+import java.io.IOException;
+
+/**
+ * Created by fengtao.xue on 2018/1/26.
+ */
+public class HbDemo {
+
+    public void main(String[] args) throws IOException {
+
+        Configuration conf = HBaseConfiguration.create();
+
+        HBaseHelper helper = HBaseHelper.getHelper(conf);
+        helper.dropTable("wctb");
+        helper.createTable("wctb", "colfam1");
+
+        Connection connection = ConnectionFactory.createConnection(conf);
+        Table table = connection.getTable(TableName.valueOf("wctb"));
+
+        //TODO something here
+        //put, get, scan
+
+        table.close();
+        connection.close();
+        helper.close();
+    }
+}
